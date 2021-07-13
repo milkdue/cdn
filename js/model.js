769549b779a13c26a6103eaba43a07b404bfc610
milkdue <69843097+milkdue@users.noreply.github.com>
2021-07-13 22:04:47 +0800

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using Terser v5.3.5.
+ * Original file: /npm/chenyfan-oss@2.0.2/index.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+function loadScript(e,n){var t=document.createElement("script"),a=document.getElementsByTagName("head")[0];t.type="text/javascript",t.charset="UTF-8",t.src=e,t.addEventListener?t.addEventListener("load",(function(){n()}),!1):t.attachEvent&&t.attachEvent("onreadystatechange",(function(){"loaded"==window.event.srcElement.readyState&&n()})),a.appendChild(t)}function loadlive2d(){document.body.clientWidth>600&&(document.onreadystatechange=function(){"complete"==document.readyState&&loadScript("https://cdn.jsdelivr.net/combine/npm/chenyfan-oss@2.0.1/pixi.min.js,npm/chenyfan-oss@2.0.1/live2dcubismcore.min.js,npm/chenyfan-oss@2.0.1/live2dcubismframework.min.js,npm/chenyfan-oss@2.0.1/live2dcubismpixi.min.js,npm/chenyfan-oss@2.0.1/load.js",(function(){loadModel()}))})}loadlive2d();
+//# sourceMappingURL=/sm/e8d2b25053ed3e14bcf4ca020cd4d8b655de282403b4059e1627356b4f1a7cc7.map
